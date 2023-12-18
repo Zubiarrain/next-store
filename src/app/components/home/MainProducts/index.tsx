2e66ce0be9e15f26a96dceedc5c519ef11aba25a
Zubiarrain <zubiarrainnahuel@gmail.com>
2023-12-18 11:27:56 -0300

    feat: llamada a api

--- src/app/components/home/MainProducts/index.tsx
+++ src/app/components/home/MainProducts/index.tsx
@@ -1,5 +1,19 @@
-export const MainProducts = () => {
-    console.log(process.env.SHOPIFY_HOSTNAME)
+const getProducts = async () => {
+    const response = await fetch(
+        `${process.env.SHOPIFY_HOSTNAME}/admin/api/2023-10/products.json`,
+        {
+            headers: {
+                "X-Shopify-Access-Token": process.env.SHOPIFY_API_KEY || ""
+            }
+        }
+    )
+    const data = await response.json()
+    return data
+}
+
+export const MainProducts = async () => {
+    const products = await getProducts()
+    console.log(products)
     return (
         <section>
             <h1>MainProducts</h1>
