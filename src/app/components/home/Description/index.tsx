770be05d8db2674af6fc61f252a2568bf869eef1
Zubiarrain <zubiarrainnahuel@gmail.com>
2023-12-18 10:04:52 -0300

    feat: optimizacion image

--- src/app/components/home/Description/index.tsx
+++ src/app/components/home/Description/index.tsx
@@ -1,17 +1,22 @@
 import styles from './Description.module.sass';
 import Image from 'next/image';
+import { PLACEHOLDER_IMAGE } from './placecholder_image';
+
 
 export const Description = () => {
   return (
     <section className={styles.Description}>
-      <Image 
-        src="/images/description.jpeg" 
-        alt="products marketplace" 
-        width={500} 
-        height={300}
-        //priority={false} // desactivar lazy loading (solo si son muy grandes)
-        //quality={100}
-    />
+      <div className={styles.Description__imageContainer}>  
+        <Image 
+          src="/images/description.jpeg" 
+          alt="products marketplace" 
+          fill
+          placeholder='blur'
+          blurDataURL= {PLACEHOLDER_IMAGE}
+          //priority={false} // desactivar lazy loading (solo si son muy grandes)
+          //quality={100}
+        />
+      </div>
       <div className={styles.Description__text}>
         <h2>Bring the future today</h2>
         <p>Future World: Your Gateway to Tomorrow's Tech! Dive into a world of cutting-edge gadgets and gear. Stay ahead of the curve and redefine your digital lifestyle with us.</p>
